9bd72c6f4992acf161f8c0a51949a5a012c15f40
Elaine <Elaine@ELAINE-PC>
2017-05-22 19:38:57 +0800

    t

--- ARV/ARV_0.1.cpp
+++ ARV/ARV_0.1.cpp
@@ -19,14 +19,16 @@ int main()
 
 	//读取原图
 
-	Mat src1 = imread("1.jpg", 1);
-	Mat src2 = imread("2.jpg", 1);
-	Mat a, b;
+	Mat src_R = imread("R.jpg", 1);
+	Mat src_C = imread("C.jpg", 1);
+	Mat src_L = imread("L.jpg", 1);
+	Mat gray_R, gray_C,gray_L;
 
 	//将原图转化成灰度图
 
-	cvtColor(src1, a, COLOR_BGR2GRAY);
-	cvtColor(src2, b, COLOR_BGR2GRAY);
+	cvtColor(src_R, gray_R, COLOR_BGR2GRAY);
+	cvtColor(src_C, gray_C, COLOR_BGR2GRAY);
+	cvtColor(src_L, gray_L, COLOR_BGR2GRAY);
 
 	Ptr<SURF> surf;
 	surf = SURF::create(800);
@@ -36,8 +38,8 @@ int main()
 	vector<KeyPoint>key1, key2;
 	vector<DMatch> matches;
 
-	surf->detectAndCompute(a, Mat(), key1, c);
-	surf->detectAndCompute(b, Mat(), key2, d);
+	surf->detectAndCompute(gray_R, Mat(), key1, c);
+	surf->detectAndCompute(gray_C, Mat(), key2, d);
 
 	matcher.match(c, d, matches);       //匹配
 
@@ -50,7 +52,7 @@ int main()
 		good_matches.push_back(matches[i]);
 	}
 	Mat outimg;
-	drawMatches(a, key1, b, key2, good_matches, outimg, Scalar::all(-1), Scalar::all(-1), vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);  //绘制匹配点
+	drawMatches(gray_R, key1, gray_C, key2, good_matches, outimg, Scalar::all(-1), Scalar::all(-1), vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);  //绘制匹配点
 
 	vector<Point2f> obj;
 	vector<Point2f> scene;
@@ -68,43 +70,43 @@ int main()
 
 	//vector<Point2f> obj_corners(4);
 	//obj_corners[0] = Point(0, 0);
-	//obj_corners[1] = Point(a.cols, 0);
-	//obj_corners[2] = Point(a.cols, a.rows);
-	//obj_corners[3] = Point(0, a.rows);
+	//obj_corners[1] = Point(gray_R.cols, 0);
+	//obj_corners[2] = Point(gray_R.cols, gray_R.rows);
+	//obj_corners[3] = Point(0, gray_R.rows);
 	//vector<Point2f> scene_corners(4);
 
 	//perspectiveTransform(obj_corners, scene_corners, H);
 
-	//line(outimg, scene_corners[0] + Point2f((float)a.cols, 0), scene_corners[1] + Point2f((float)a.cols, 0), Scalar(0, 255, 0), 2, LINE_AA);       //绘制
-	//line(outimg, scene_corners[1] + Point2f((float)a.cols, 0), scene_corners[2] + Point2f((float)a.cols, 0), Scalar(0, 255, 0), 2, LINE_AA);
-	//line(outimg, scene_corners[2] + Point2f((float)a.cols, 0), scene_corners[3] + Point2f((float)a.cols, 0), Scalar(0, 255, 0), 2, LINE_AA);
-	//line(outimg, scene_corners[3] + Point2f((float)a.cols, 0), scene_corners[0] + Point2f((float)a.cols, 0), Scalar(0, 255, 0), 2, LINE_AA);
+	//line(outimg, scene_corners[0] + Point2f((float)gray_R.cols, 0), scene_corners[1] + Point2f((float)gray_R.cols, 0), Scalar(0, 255, 0), 2, LINE_AA);       //绘制
+	//line(outimg, scene_corners[1] + Point2f((float)gray_R.cols, 0), scene_corners[2] + Point2f((float)gray_R.cols, 0), Scalar(0, 255, 0), 2, LINE_AA);
+	//line(outimg, scene_corners[2] + Point2f((float)gray_R.cols, 0), scene_corners[3] + Point2f((float)gray_R.cols, 0), Scalar(0, 255, 0), 2, LINE_AA);
+	//line(outimg, scene_corners[3] + Point2f((float)gray_R.cols, 0), scene_corners[0] + Point2f((float)gray_R.cols, 0), Scalar(0, 255, 0), 2, LINE_AA);
 
 
-	namedWindow("H", WINDOW_NORMAL);
-	imshow("H", H);
-	imshow("aaaa", outimg);
-	cout << "H=  (PYTHON)" << endl << format(H, Formatter::FMT_PYTHON) << endl << endl;
+	/*namedWindow("H", WINDOW_NORMAL);
+	imshow("H", H);*/
+	imshow("SRC", outimg);
+	/*cout << "H=  (PYTHON)" << endl << format(H, Formatter::FMT_PYTHON) << endl << endl;*/
 
-	waitKey(0);
+	
 
 	//拼接
 
 
 	int e = 30;//渐入渐出融合宽度
 	Mat result;
-	while (1) {
+	/*while (1) {*/
 		start = clock();
 		//cols　列 rows 行
-		warpPerspective(src1, result, H, Size(2 * src1.cols - e, src1.rows));//Size设置结果图像宽度，宽度裁去一部分，e可调
+		warpPerspective(src_R, result, H, Size(2 * src_R.cols - e, src_R.rows));//Size设置结果图像宽度，宽度裁去一部分，e可调
 		imshow("result", result);
-		Mat half(result, Rect(0, 0, src2.cols - e, src2.rows));
+		Mat half(result, Rect(0, 0, src_C.cols - e, src_C.rows));
 		imshow("half", half);
-		src2(Range::all(), Range(0, src2.cols - e)).copyTo(half);
+		src_C(Range::all(), Range(0, src_C.cols - e)).copyTo(half);
 		imshow("half2", half);
 		for (int i = 0; i < e; i++)
 		{
-			result.col(src2.cols - e + i) = (e - i) / (float)e*src2.col(src2.cols - e + i) + i / (float)e*result.col(src2.cols - e + i);  //权重
+			result.col(src_C.cols - e + i) = (e - i) / (float)e*src_C.col(src_C.cols - e + i) + i / (float)e*result.col(src_C.cols - e + i);  //权重
 		}
 
 		imshow("stitch", result);
@@ -114,7 +116,7 @@ int main()
 
 		printf("%f ms\n", duration);
 		waitKey(20);
-	}
+	/*}*/
 
 
 
