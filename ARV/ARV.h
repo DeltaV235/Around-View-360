ef6dcc3fcce2e28779fb9f63ca01e5dbc59cc393
Elaine <Elaine@ELAINE-PC>
2017-05-23 20:41:02 +0800

    创建stitch类

--- ARV/ARV.h
+++ ARV/ARV.h
@@ -10,4 +10,103 @@ using namespace cv;
 using namespace std;
 using namespace cv::xfeatures2d;
 
-Mat findH(Mat src_L, Mat src_R);
+class StitchForVideo {
+private:
+	clock_t start, finish;
+	Mat src_L, src_R, gray_R, gray_L, c, d, outimg, H;
+	Ptr<SURF> surf = SURF::create(800);
+	BFMatcher matcher;
+	vector<KeyPoint>key1, key2;
+	vector<DMatch> matches, good_matches;
+	int ptsPairs = 0;
+	vector<Point2f> obj, scene;
+
+public:
+	int setSRC_L(string path)
+	{
+		src_L = imread(path, 1);
+		return true;
+	}
+
+	int setSRC_R(string path)
+	{
+		src_R = imread(path, 1);
+		return true;
+	}
+
+	Mat findH(string path)
+	{
+		start = clock();
+		//将原图转化成灰度图
+		cvtColor(src_R, gray_R, COLOR_BGR2GRAY);
+		cvtColor(src_L, gray_L, COLOR_BGR2GRAY);
+		surf->detectAndCompute(gray_R, Mat(), key1, c);
+		surf->detectAndCompute(gray_L, Mat(), key2, d);
+		//匹配
+		matcher.match(c, d, matches);
+		//筛选匹配点
+		sort(matches.begin(), matches.end());
+
+		ptsPairs = min(50, (int)(matches.size() * 0.15));
+		cout << ptsPairs << endl;
+		for (int i = 0; i < ptsPairs; i++)
+		{
+			good_matches.push_back(matches[i]);
+		}
+		//绘制匹配点
+		drawMatches(gray_R, key1, gray_L, key2, good_matches, outimg, 
+			Scalar::all(-1), Scalar::all(-1), vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+
+		for (size_t i = 0; i < good_matches.size(); i++)
+		{
+			obj.push_back(key1[good_matches[i].queryIdx].pt);
+			scene.push_back(key2[good_matches[i].trainIdx].pt);
+		}
+		//寻找匹配的图像
+		H = findHomography(obj, scene, RANSAC);
+		finish = clock();
+		cout << "findH: " << (double)(finish - start) << " ms" << endl;
+
+
+		imwrite(path, H);
+		return H;
+	}
+
+	void show()
+	{
+		vector<Point2f> obj_corners(4);
+		obj_corners[0] = Point(0, 0);
+		obj_corners[1] = Point(gray_R.cols, 0);
+		obj_corners[2] = Point(gray_R.cols, gray_R.rows);
+		obj_corners[3] = Point(0, gray_R.rows);
+		vector<Point2f> scene_corners(4);
+
+		perspectiveTransform(obj_corners, scene_corners, H);
+
+		line(outimg, scene_corners[0] + Point2f((float)gray_R.cols, 0), scene_corners[1] + Point2f((float)gray_R.cols, 0), Scalar(0, 255, 0), 2, LINE_AA);       //绘制
+		line(outimg, scene_corners[1] + Point2f((float)gray_R.cols, 0), scene_corners[2] + Point2f((float)gray_R.cols, 0), Scalar(0, 255, 0), 2, LINE_AA);
+		line(outimg, scene_corners[2] + Point2f((float)gray_R.cols, 0), scene_corners[3] + Point2f((float)gray_R.cols, 0), Scalar(0, 255, 0), 2, LINE_AA);
+		line(outimg, scene_corners[3] + Point2f((float)gray_R.cols, 0), scene_corners[0] + Point2f((float)gray_R.cols, 0), Scalar(0, 255, 0), 2, LINE_AA);
+		imshow("test for findH", outimg);
+
+	}
+
+	Mat stitch(int width)
+	{
+		clock_t start, finish;
+		start = clock();
+		Mat result;
+		//cols　列 rows 行
+		warpPerspective(src_R, result, H, Size(2 * src_R.cols - width, src_R.rows));//Size设置结果图像宽度，宽度裁去一部分，e可调
+
+		Mat half(result, Rect(0, 0, src_L.cols - width, src_L.rows));
+		src_L(Range::all(), Range(0, src_L.cols - width)).copyTo(half);
+		for (int i = 0; i < width; i++)
+		{
+			result.col(src_L.cols - width + i) = (width - i) / (float)width*src_L.col(src_L.cols - width + i) + i / (float)width*result.col(src_L.cols - width + i);  //权重
+		}
+		finish = clock();
+		printf("stitch: %.2f ms\n", (double)(finish - start));
+		return result;
+	}
+};
--- ARV/MAIN.cpp
+++ ARV/MAIN.cpp
@@ -17,12 +17,12 @@ int main()
 
 	//读取原图
 
-	Mat src_R = imread("R.jpg", 1);
-	Mat src_C = imread("C.jpg", 1);
-	Mat src_L = imread("L.jpg", 1);
+	Mat src_R = imread("1R.jpg", 1);
+	Mat src_C = imread("1C.jpg", 1);
+	Mat src_L = imread("1L.jpg", 1);
 	Mat gray_R, gray_C,gray_L;
 
-	//将原图转化成灰度图
+	
 	start = clock();
 
 	cvtColor(src_R, gray_R, COLOR_BGR2GRAY);
@@ -42,10 +42,10 @@ int main()
 	surf->detectAndCompute(gray_C, Mat(), key2, d);
 	surf->detectAndCompute(gray_L, Mat(), key3, e);
 
-	matcher.match(c, d, matches_CR);       //匹配
+	matcher.match(c, d, matches_CR);       
 	matcher.match(d, e, matches_LC);
 
-	sort(matches_CR.begin(), matches_CR.end());  //筛选匹配点
+	sort(matches_CR.begin(), matches_CR.end()); 
 	sort(matches_LC .begin(), matches_LC.end());
 
 
@@ -71,7 +71,7 @@ int main()
 
 
 	Mat outimg_CR;
-	drawMatches(gray_R, key1, gray_C, key2, good_matches_CR, outimg_CR, Scalar::all(-1), Scalar::all(-1), vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);  //绘制匹配点
+	drawMatches(gray_R, key1, gray_C, key2, good_matches_CR, outimg_CR, Scalar::all(-1), Scalar::all(-1), vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS); 
 
 	Mat outimg_LC;
 	drawMatches(gray_C, key2, gray_L, key3, good_matches_LC, outimg_LC, Scalar::all(-1), Scalar::all(-1), vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);  //绘制匹配点
@@ -89,7 +89,7 @@ int main()
 	
 	
 
-	Mat H_CR = findHomography(obj, scene, RANSAC);      //寻找匹配的图像
+	Mat H_CR = findHomography(obj, scene, RANSAC);      
 
 	obj.clear();
 	scene.clear();
@@ -101,41 +101,37 @@ int main()
 	Mat H_LC = findHomography(obj, scene, RANSAC);
 	finish = clock();
 	duration = (double)(finish - start);
-	printf("%f ms\n", duration);
-	findH(src_L, src_C);
-	//vector<Point2f> obj_corners(4);
-	//obj_corners[0] = Point(0, 0);
-	//obj_corners[1] = Point(gray_R.cols, 0);
-	//obj_corners[2] = Point(gray_R.cols, gray_R.rows);
-	//obj_corners[3] = Point(0, gray_R.rows);
-	//vector<Point2f> scene_corners(4);
-
-	//perspectiveTransform(obj_corners, scene_corners, H);
-
-	//line(outimg, scene_corners[0] + Point2f((float)gray_R.cols, 0), scene_corners[1] + Point2f((float)gray_R.cols, 0), Scalar(0, 255, 0), 2, LINE_AA);       //绘制
-	//line(outimg, scene_corners[1] + Point2f((float)gray_R.cols, 0), scene_corners[2] + Point2f((float)gray_R.cols, 0), Scalar(0, 255, 0), 2, LINE_AA);
-	//line(outimg, scene_corners[2] + Point2f((float)gray_R.cols, 0), scene_corners[3] + Point2f((float)gray_R.cols, 0), Scalar(0, 255, 0), 2, LINE_AA);
-	//line(outimg, scene_corners[3] + Point2f((float)gray_R.cols, 0), scene_corners[0] + Point2f((float)gray_R.cols, 0), Scalar(0, 255, 0), 2, LINE_AA);
+	printf("%.2f ms\n", duration);
+	
+	StitchForVideo stitch;
+	stitch.setSRC_L("1C.jpg");
+	stitch.setSRC_R("1R.jpg");
+	stitch.findH("H_LR.jpg");
+	stitch.show();
+	
+	
 
 
 	/*namedWindow("H", WINDOW_NORMAL);
 	imshow("H", H);*/
-	//imshow("SRC_CR", outimg_CR);
-	imshow("SRC_LC", outimg_LC);
+	imshow("SRC_CR", outimg_CR);
+	//imshow("SRC_LC", outimg_LC);
 	/*cout << "H=  (PYTHON)" << endl << format(H, Formatter::FMT_PYTHON) << endl << endl;*/
 
 	
 
 	//拼接
-
-
+	imshow("stitch_CR", stitch.stitch(50));
+	clock_t begin, end;
+	
 	int width = 30;//渐入渐出融合宽度
 	Mat result,result_LC;
 	/*while (1) {*/
 		
+	begin = clock();
 		//cols　列 rows 行
 		warpPerspective(src_R, result,H_CR, Size(3 * src_R.cols - width, src_R.rows));//Size设置结果图像宽度，宽度裁去一部分，e可调
-		//imshow("result", result);
+		
 		Mat half(result, Rect(0, 0, src_C.cols - width, src_C.rows));
 		//imshow("half", half);
 		src_C(Range::all(), Range(0, src_C.cols - width)).copyTo(half);
@@ -144,9 +140,15 @@ int main()
 		{
 			result.col(src_C.cols - width + i) = (width - i) / (float)width*src_C.col(src_C.cols - width + i) + i / (float)width*result.col(src_C.cols - width + i);  //权重
 		}
+	end = clock();
+	printf("%.2f ms\n", (double)(end - begin));
+
+		imshow("result", result);
+		
+		
 
 
-
+		
 
 		warpPerspective(result, result_LC, H_LC, Size(4 * src_L.cols - width, src_L.rows));//Size设置结果图像宽度，宽度裁去一部分，e可调
 		//imshow("result_LC", result_LC);
@@ -159,9 +161,10 @@ int main()
 			result_LC.col(src_L.cols - width + i) = (width - i) / (float)width*src_L.col(src_L.cols - width + i) + i / (float)width*result_LC.col(src_L.cols - width + i);  //权重
 		}
 
+		
 
 		//imshow("stitch", result);
-		imshow("stitch", result_LC);
+		//imshow("stitch", result_LC);
 		
 		
 		waitKey(20);
