db2b90352b76596bc2e2b3069edfcc6a46469038
wuyue96 <wuyue96@gmail.com>
2017-10-03 13:29:28 +0800

    无效

--- ARV/ARV.h
+++ ARV/ARV.h
@@ -1,10 +1,11 @@
-#pragma once
+//#pragma once
 #include "iostream"
 #include "opencv2/highgui.hpp"
 #include "opencv2/calib3d.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/xfeatures2d.hpp"
 #include "time.h"
+
 
 #define SHOW_TIME 1
 #define TIME_ARR_SIZE 5
