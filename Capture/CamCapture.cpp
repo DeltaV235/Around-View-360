579f00a42c66dddbec0de77806c542d7d0907870
wuyue96 <wuyue96@gmail.com>
2017-12-22 20:09:19 +0800

    完成了多个摄像头的同时采集，并且将视频文件保存在一个以时间命名的文件夹中

--- Capture/CamCapture.cpp
+++ Capture/CamCapture.cpp
@@ -1,10 +1,11 @@
 #include "stdafx.h"
 #include "CamCapture.h"
 
+#define camMaxNum 6
 
-CamCapture & CamCapture::capture(int camNum, int width, int heigth, double fps, string savePath)
+CamCapture & CamCapture::capture(int camSeq, int width, int heigth, double fps, string savePath)
 {
-	VideoCapture capture(camNum);
+	VideoCapture capture(camSeq);
 	VideoWriter output;
 	int count = 0;
 	Mat frameImg;
@@ -18,6 +19,7 @@ CamCapture & CamCapture::capture(int camNum, int width, int heigth, double fps, 
 	output.open(savePath, CV_FOURCC('D', 'I', 'V', 'X'), fps, videoResolution, true);
 	if (!output.isOpened())
 		cout << "Fail to open file !" << endl;
+	waitKey(1000);
 	while (true)
 	{
 		capture >> frameImg;
@@ -40,4 +42,57 @@ CamCapture & CamCapture::capture(int camNum, int width, int heigth, double fps, 
 	}
 	return *this;
 	// TODO: 在此处插入 return 语句
+}
+
+
+CamCapture & CamCapture::capture(unsigned int  camNum, int width, int heigth, double fps, char saveDirName[])
+{
+	VideoCapture *cam[camMaxNum];
+	Size videoResolution[camMaxNum];
+	VideoWriter output[camMaxNum];
+	char fileName[80];
+	int count[camMaxNum] = { 0 };
+	Mat frameImg;
+	for (int i = 0; i < camNum; i++)
+	{
+		cam[i] = new VideoCapture(i);
+		cam[i]->set(CV_CAP_PROP_FRAME_WIDTH, width);
+		cam[i]->set(CV_CAP_PROP_FRAME_HEIGHT, heigth);
+		cam[i]->set(CV_CAP_PROP_FPS, fps);
+		videoResolution[i] = Size((int)cam[i]->get(CV_CAP_PROP_FRAME_WIDTH), (int)cam[i]->get(CV_CAP_PROP_FRAME_HEIGHT));		//获取视频分辨率
+		if (!cam[i]->isOpened())
+			cout << "Cam " << i << " hasnt opened !" << endl;
+		sprintf(fileName, "%s\\Cam%d.avi", saveDirName, i);
+		output[i].open(fileName, CV_FOURCC('D', 'I', 'V', 'X'), fps, videoResolution[i], true);
+		if (!output[i].isOpened())
+			cout << "Fail to open file " << i << " !" << endl;
+	}
+	waitKey(1000);
+
+	while (true)
+	{
+		for (int i = 0; i < camNum; i++) 
+		{
+			*cam[i] >> frameImg;
+			if (!frameImg.empty())
+			{
+				count[i]++;
+				output[i] << frameImg;
+				sprintf(fileName, "%s\\Cam%d.avi", saveDirName, i);
+				imshow(fileName, frameImg);
+			}
+			else
+			{
+				cout << "Fail to save frame for Cam " << i << " !" << endl;
+				break;
+			}
+		}
+		if (char(waitKey(1)) == 'q')
+		{
+			for (int i = 0; i < camNum; i++)
+				cout << "writeTotalFrame for Cam " << i << " : " << count[i] << endl;
+			break;
+		}
+	}
+	return *this;
 }
--- Capture/CamCapture.h
+++ Capture/CamCapture.h
@@ -10,6 +10,6 @@ private:
 	//VideoCapture videoCapture(0);
 public:
 	CamCapture& videoSave();
-	CamCapture& capture(int camNum, int width, int heigth, double fps, string savePath);
-
+	CamCapture& capture(int camSeq, int width, int heigth, double fps, string savePath);		//单摄像头采集视频 savePath为保存的路径
+	CamCapture& capture(unsigned int  camNum, int width, int heigth, double fps, char saveDirName[]);	//多摄像头同时采集视频 saveDirPath为保存的文件夹路径
 };
